84402c3b0eca18144ee43fdedd59e16a65da3a76
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-05 18:44:28 +0000

    js variable let, const and var

---
+++ basics/01variable.js
@@ -0,0 +1,14 @@
+const userId = 5432
+let emailId = "kc@gmail.com"
+var userName = "kabichaudhary"
+region = "asia nepal"
+
+// console.log(userId);
+
+
+/*
+Do not use var
+because of issue in blcok scope and functional scope.
+*/
+
+console.table([userId, userName, emailId, region]);
