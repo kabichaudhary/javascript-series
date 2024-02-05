08f4d0f42d96bddda4bc21a80d9838798f861d22
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-05 18:50:00 +0000

    changes in js variable

--- basics/01variable.js
+++ basics/01variable.js
@@ -2,6 +2,8 @@ const userId = 5432
 let emailId = "kc@gmail.com"
 var userName = "kabichaudhary"
 region = "asia nepal"
+let userState;
+
 
 // console.log(userId);
 
@@ -11,4 +13,4 @@ Do not use var
 because of issue in blcok scope and functional scope.
 */
 
-console.table([userId, userName, emailId, region]);
+console.table([userId, userName, emailId, region, userState]);
