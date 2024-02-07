2085d78048aa24a541d60673d8a733d8d60e286a
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-07 18:36:16 +0000

    js data types

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+const number = 125; // => 2 to power 53
+const string = ''
+const age = null
+
+// const undefined 
+// boolean => true or false
+// null =? standlone value
+// symbol
+// object
+
+console.log(typeof "kabi");
+
