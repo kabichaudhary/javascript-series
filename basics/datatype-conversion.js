73b020937fc33e31e4f9e12b6f69599d44afb249
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-07 19:09:04 +0000

    data type conversion

---
+++ basics/datatype-conversion.js
@@ -0,0 +1,34 @@
+let grade = "6.5"  //=> numebr
+
+let grades = "6.5abc" //=> NaN ----> not a number
+
+
+let valueInNumber = Number(grade)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let accountBalance = 7000
+
+let valueInString = String(accountBalance)
+
+// console.log(typeof valueInString);
+
+// console.log(valueInString);
+
+
+// --------Boolean Conversion------
+
+let number = 1
+
+let booleanValue = Boolean(number)
+
+console.log(typeof booleanValue);
+
+console.log(booleanValue);
+
+//  1-> true
+// 0 -> false
+// "" -> false
+// "kabi" -> true
