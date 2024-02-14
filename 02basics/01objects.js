9f52d8073bc811f2a5a74022b2d84c564d618151
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-14 19:09:04 +0000

    object is done!

---
+++ 02basics/01objects.js
@@ -0,0 +1,28 @@
+// singleton
+// Object.create()
+
+// Object literal
+
+const mySymbol = Symbol("key1")
+
+const jsDeveloper = {
+    name: "kabi",
+    age: 24,
+    [mySymbol]: "kayvalue1",
+    "full_name": "Kabi Chaudhary",
+    location: "nepal",
+    email: "kabi.google.com",
+    isLogedDays: false,
+}
+
+console.log(jsDeveloper.name);
+console.log(jsDeveloper["name"]);
+console.log(jsDeveloper["full_name"]);
+console.log(jsDeveloper.age);
+console.log(typeof jsDeveloper[mySymbol]);
+
+// Accessing objects value
+jsDeveloper.email = "kabi.scalar.com"  //override the key values
+console.log(jsDeveloper.email);  
+Object.freeze(jsDeveloper)  //lock the object
+jsDeveloper.email = "kabi.chatgpt.com"
