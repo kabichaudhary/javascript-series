3026167949727b2590c2a26af035ad3b2b04a8c1
kabichaudhary <156126771+kabichaudhary@users.noreply.github.com>
2024-02-20 17:10:36 +0000

    objects basics !!

--- 02basics/01objects.js
+++ 02basics/01objects.js
@@ -15,14 +15,24 @@ const jsDeveloper = {
     isLogedDays: false,
 }
 
-console.log(jsDeveloper.name);
-console.log(jsDeveloper["name"]);
-console.log(jsDeveloper["full_name"]);
-console.log(jsDeveloper.age);
-console.log(typeof jsDeveloper[mySymbol]);
+// console.log(jsDeveloper.name);
+// console.log(jsDeveloper["name"]);
+// console.log(jsDeveloper["full_name"]);
+// console.log(jsDeveloper.age);
+// console.log(typeof jsDeveloper[mySymbol]);
 
 // Accessing objects value
 jsDeveloper.email = "kabi.scalar.com"  //override the key values
-console.log(jsDeveloper.email);  
-Object.freeze(jsDeveloper)  //lock the object
+// console.log(jsDeveloper.email);  
+// Object.freeze(jsDeveloper)  //lock the object
 jsDeveloper.email = "kabi.chatgpt.com"
+
+jsDeveloper.greeting = function(){
+    console.log("hello js developers!!");
+}
+jsDeveloper.greetingTwo = function(){
+    console.log(`Hello jsDeveloper ${this.name}`);
+}
+console.log(jsDeveloper.greeting());
+console.log(jsDeveloper.greetingTwo());
+
